9dfc8d5dbe710d3a5f5b923dbcc7ad0f32222ba2
Mark Soh <marksoh115@gmail.com>
2025-08-20 17:29:03 -0700

    added new images

--- app/pokemonGo/page.js
+++ app/pokemonGo/page.js
@@ -71,7 +71,7 @@ export default function PokemonGoProject() {
                     Before
                 </figcaption>
                 <Image
-                    src={getImagePath("/Proj3-Img/PokemonBefore.webp")}
+                    src={getImagePath("/Proj3-Img/PokemonBefore.png")}
                     alt="Before - Pokémon Type Calculator"
                     className="rounded-lg shadow-lg"
                     width={800}   
@@ -84,7 +84,7 @@ export default function PokemonGoProject() {
                     After
                 </figcaption>
                 <Image
-                    src={getImagePath("/Proj3-Img/PokemonAfter.webp")}
+                    src={getImagePath("/Proj3-Img/PokemonAfter.png")}
                     alt="After - Pokémon Type Calculator"
                     className="rounded-lg shadow-lg"
                     width={800}  
